cb26460be533e3e5034396aea652796ab42aee2e
Sam <29763502+malmas18@users.noreply.github.com>
2019-01-24 10:12:36 +1100

    update assessment

--- app/main/views/case_study.py
+++ app/main/views/case_study.py
@@ -54,29 +54,35 @@ def case_study_assessment_list():
 def case_study_assessment_search():
     search = request.args.get('search', None)
     response = data_api_client.req.admin().casestudy().assessment().get({
-        'search':search
+        'search': search
     })
     case_studies = response.get('case_studies')
-    
+
     return jsonify(case_studies)
 
 
-@main.route('/casestudy-assessment/<int:case_study_id>', methods=['GET'])
+@main.route('/casestudy/<int:case_study_id>/assessment', methods=['GET'])
+@main.route('/casestudy/<int:case_study_id>/assessment/<int:case_study_assessment_id>', methods=['GET'])
 @login_required
 @role_required('admin')
-def case_study_view(case_study_id):
+def case_study_view(case_study_id, case_study_assessment_id=None):
+    role = request.args.get('role', None)
     response = data_api_client.req.admin().casestudy(case_study_id).get()
     case_study = response.get('case_study')
     domain = response.get('domain')
-
-    case_study['admin'] = True
     case_study['domain'] = domain
 
     rendered_component = render_component(
         'bundles/CaseStudy/CaseStudyViewWidget.js', {
             'casestudy': case_study,
             'meta': {
-                'url_case_study_assessment_update': url_for('.add_case_study_assessment', case_study_id=case_study_id),
+                'url_case_study_assessment_add': url_for('.add_case_study_assessment', case_study_id=case_study_id),
+                'url_case_study_assessment_update': url_for(
+                    '.update_case_study_assessment',
+                    case_study_id=case_study_id,
+                    case_study_assessment_id=case_study_assessment_id
+                ) if case_study_assessment_id else None,
+                'role': role
             }
         }
     )
@@ -99,6 +105,28 @@ def add_case_study_assessment(case_study_id):
         .casestudy(case_study_id)
         .assessment()
         .post({
+            'update_details': {
+                'updated_by': current_user.email_address
+            },
+            "assessor_user_id": json_payload.get('assessor_user_id')
+        })
+    )
+
+    return jsonify(result)
+
+
+@main.route('/casestudy/<int:case_study_id>/assessment/<int:case_study_assessment_id>', methods=['PUT'])
+@login_required
+@role_required('admin')
+def update_case_study_assessment(case_study_id, case_study_assessment_id):
+    json_payload = request.get_json(force=True)
+    result = (
+        data_api_client
+        .req
+        .admin()
+        .casestudy(case_study_id)
+        .assessment(case_study_assessment_id)
+        .put({
             'update_details': {
                 'updated_by': current_user.email_address
             },
