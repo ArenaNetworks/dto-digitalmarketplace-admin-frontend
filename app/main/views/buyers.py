52bf238e6de7f6883e37b6dccfcdd6dbe9c155a6
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2020-11-25 14:30:13 +1100

    Update 'platform' to 'platforms'

--- app/main/views/buyers.py
+++ app/main/views/buyers.py
@@ -21,7 +21,7 @@ AREA_OF_EXPERTISE_LIST = [
     'ICT risk management and audit activities',
     'ICT systems integration',
     'Marketing, Communications and Engagement',
-    'Platform integration',
+    'Platforms integration',
     'Software engineering and Development',
     'Strategy and Policy',
     'Support and Operations',
